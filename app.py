f010495525541642fb55c5e84d51833313ebd1ca
Hithallo Sousa Santos <hsantos.developer@gmail.com>
2025-07-06 19:36:03 -0300

    Add password encryption for user accounts

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from database import db
 from models.user import User
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -28,7 +29,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
@@ -50,7 +51,8 @@ def create_user():
     password = data.get("password")
     
     if username and password:
-        user = User(username=username, password=password, role='user')
+        hased_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hased_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário Cadastrado com Sucesso!"})
